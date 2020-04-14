25d77001836b65a1784611d9d1d2fa15dd4b991f
Wilfried Canirinka <canirinkawilfried@gmail.com>
2020-04-14 21:59:31 +0200

    Lint errors corrected

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 /* eslint-disable import/no-unresolved */
-import express from 'express';
-import serveStatic from 'serve-static';
-import { join } from 'path';
+const express = require('express');
+const serveStatic = require('serve-static');
+const path = require('path');
 
 const app = express();
-app.use('/', serveStatic(join(__dirname, '/build')));
+app.use('/', serveStatic(path.join(__dirname, '/build')));
 app.get(/.*/, (req, res) => {
-  res.sendFile(join(__dirname, '/build/index.html'));
+  res.sendFile(path.join(__dirname, '/build/index.html'));
 });
 const port = process.env.PORT || 8080;
 app.listen(port);
